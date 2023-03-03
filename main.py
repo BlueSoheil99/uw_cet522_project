0d284acc43e98444de3c8b314c99d96e120bc86c
Bluesoheil99 <soheil.k.78@gmail.com>
2023-03-02 16:45:36 -0800

    created a framework and shared some codes to start with in a .ipynb file

---
+++ main.py
@@ -0,0 +1,21 @@
+import logic
+import database
+import application
+
+
+# run application
+
+
+# get input from user
+    # a list of inputs.
+    # The list starts with a code by which we know what functinos to run and how to interpret the input
+
+
+# get data from database
+
+
+# run the request
+req_code = 1
+route = 'I5'
+
+# respond to the application
