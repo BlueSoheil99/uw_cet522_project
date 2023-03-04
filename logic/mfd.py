bbb145ba59ff66f04f365211c15bd053f14480e9
Shakiba97 <122954016+Shakiba97@users.noreply.github.com>
2023-03-04 13:53:46 -0800

    update

---
+++ logic/mfd.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import data_handler.fetch_data as fetch_data
+
+
+def make_MFD(route, mile_start, mile_end, Week_of_the_year):
+    data = fetch_data.abc()
+    # process data
+    plot = 'a plot you just made!'
+    return plot
+
+
+make_MFD('I5', 0, 190)
+
