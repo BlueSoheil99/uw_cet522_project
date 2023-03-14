ec0d550c4029f53543690b4a1d6fa8db2420cc98
Bluesoheil99 <soheil.k.78@gmail.com>
2023-03-13 21:06:11 -0700

    date and times are in correct format and now daily data can be read from daily data files. filtering date and time is remaining

--- data_handler/data_query.py
+++ data_handler/data_query.py
@@ -1,9 +1,27 @@
 import os
 import pandas as pd
+import numpy as np
 from . import meta_creator
+from datetime import date, datetime
+
+START_DATE_DEFAULT = date(2016, 1, 1)
+END_DATE_DEFAULT = date(2016, 12, 31)
+START_TIME_DEFAULT = '00:00'
+END_TIME_DEFAULT = '23:59'
 
 
-def _select_files(route, direction, daily_average=True, weekend=False, start_date='2016-01-01', end_date='2016-12-31'):
+def _filter_date_months(dataframe, column_names, dates):
+    start_cname, end_cname = column_names[0], column_names[1]
+    # start_date, end_date = dates[0], dates[1]  # start should be prior to end. front end handles that
+    start_month, end_month = dates[0].month, dates[1].month
+    x = dataframe[start_cname].apply(lambda date: date.month) == start_month
+    y = dataframe[end_cname].apply(lambda date: date.month) == end_month
+    first = np.argwhere(np.array(x))[0][0]
+    second = np.argwhere(np.array(y))[-1][0]
+    return dataframe.iloc[first:second+1, :]
+
+
+def _select_files(route, direction, daily_average, weekend, start_date, end_date):
     df = metadata[metadata.route == route]
     df = df[df.weekend == weekend]
     df = df[df.direction == direction]
@@ -11,17 +29,19 @@ def _select_files(route, direction, daily_average=True, weekend=False, start_dat
         df = df[df.type == 'averaged daily data']
     else:
         df = df[df.type == 'daily data']
-    # todo handling dates, which should be implemented in the end of this function, and prevent duplicates
+    df = _filter_date_months(df, ('start_date', 'end_date'), (start_date, end_date))
     df.reset_index(drop=True, inplace=True)
     return df
 
 
-def _extract_mileposts(dataframes, column_names, mileposts, periods, daily_average=True):
+def _extract_mileposts(dataframes, column_names, mileposts, periods, daily_average):
     # in case column_names are : ['time', 'start_date', 'end_date', 'speed', 'Volume (5-mins all lanes)']
     template = {col: [] for col in column_names}
-    original_first_column = dataframes[0][0].iloc[:, 0]  # todo convert it to datetime dtype
+    original_first_column = dataframes[0][0].iloc[:, 0]
     # for average daily files it's just time, for daily files, it's date and time
+
     if daily_average:
+        original_first_column = original_first_column.apply(lambda t: datetime.strptime(t, '%H:%M').time())
         template[column_names[0]] = list(original_first_column)*len(dataframes)
         # if we use daily data, we need another way to insert dates
         start, end = [], []
@@ -32,9 +52,13 @@ def _extract_mileposts(dataframes, column_names, mileposts, periods, daily_avera
             end.extend(b)
         template[column_names[1]] = start
         template[column_names[2]] = end
-    # else: # todo what if we want to use daily data instead of average data?
-    DFs = [template] * len(mileposts)
+    else:
+        original_first_column = original_first_column.apply(lambda dt: datetime.strptime(dt, '%Y-%m-%d %H:%M:%S'))
+        template[column_names[0]] = original_first_column.apply(lambda dt: dt.time())
+        template[column_names[1]] = original_first_column.apply(lambda dt: dt.date())
+        template[column_names[2]] = template[column_names[1]]
 
+    DFs = [template] * len(mileposts)
     for sheets in dataframes:
         for i, df in enumerate(sheets):
             attr = column_names[i+3]
@@ -57,31 +81,33 @@ def _read_whole_excel(path, sheet_names):
     return sheets
 
 
-def get_data(route, direction, mileposts=None, daily_average=True, weekend=False, start_date='2016-01-01', end_date='2016-12-31'):
+def get_data(route, direction, mileposts=None, daily_average=True, weekend=False,
+             start_date=START_DATE_DEFAULT, end_date=END_DATE_DEFAULT,
+             start_time=START_TIME_DEFAULT, end_time=END_TIME_DEFAULT):
     dataframes = []
     files_dates = []
     sheet_names = ['Speed', 'Volume (5-mins all lanes)']
     column_names = ['time', 'start_date', 'end_date', 'speed', 'Volume (5-mins all lanes)']
 
-    file_metas = _select_files(route, direction, daily_average, weekend)
+    file_metas = _select_files(route, direction, daily_average, weekend, start_date, end_date)
     for idx, meta in file_metas.iterrows():
-        # dataframes.append(_read_whole_excel(meta.file_adr, sheet_names))
-        # files_dates.append((meta.start_date, meta.end_date))
-        # # instead of two lines above, we save the files we read in RAM and when they are needed again, we'll use
+        # # we save the files we read in RAM and when they are needed again, we'll use
         # # previously opened files, instead of reading those files again
-        address = meta.file_adr  #meta is a row of meta table
+        address = meta.file_adr  # meta is a row of meta table
         if address in opened_DFs.keys():
             dfs, dates = opened_DFs[address][0], opened_DFs[address][1]
-            dataframes.append(dfs)
-            files_dates.append(dates)
             print(f'retrieved file: {address}')
         else:
             dfs = _read_whole_excel(meta.file_adr, sheet_names)
             dates = (meta.start_date, meta.end_date)
-            dataframes.append(dfs)
-            files_dates.append(dates)
             opened_DFs[address] = [dfs, dates]
-    DFs_for_mileposts = _extract_mileposts(dataframes, column_names, mileposts, files_dates)
+
+        dataframes.append(dfs)
+        files_dates.append(dates)
+
+    DFs_for_mileposts = _extract_mileposts(dataframes, column_names, mileposts, files_dates, daily_average)
+    # todo filter dates
+    # todo fiter time
     return DFs_for_mileposts
 
 
--- data_handler/meta_creator.py
+++ data_handler/meta_creator.py
@@ -2,6 +2,7 @@ import glob
 import pandas as pd
 import os
 import random
+from datetime import datetime
 
 
 def _define_route(route):
@@ -31,7 +32,7 @@ def _get_metadata(file_name):
     return route, direction, mile_start, mile_end, weekend, start, end
 
 
-def _create_meta(glob_address='.\\data\\DriveNet\\*\\*.xlsx'):
+def _create_meta(glob_address='./data/DriveNet/*/*.xlsx'):
     print('metacreator dir: '+os.getcwd())
     file_names = sorted(glob.glob(glob_address))
 
@@ -40,7 +41,7 @@ def _create_meta(glob_address='.\\data\\DriveNet\\*\\*.xlsx'):
     # for i, name in enumerate(file_names):
     for name in file_names:
         org_name = name
-        name = name.split('\\')
+        name = name.split('/')
         pre_name = name[:-1]
         name = name[-1][:-5]  # remove '.xlsx' part
         route, direction, mile_start, mile_end, weekend, start, end = _get_metadata(name)
@@ -52,8 +53,8 @@ def _create_meta(glob_address='.\\data\\DriveNet\\*\\*.xlsx'):
         info['mile_start'].append(mile_start)
         info['mile_end'].append(mile_end)
         info['direction'].append(direction)
-        info['start_date'].append(start)
-        info['end_date'].append(end)
+        info['start_date'].append(datetime.strptime(start, '%Y-%m-%d').date())
+        info['end_date'].append(datetime.strptime(end, '%Y-%m-%d').date())
         if weekend == 'MoTuWeThFr':
             info['weekend'].append(False)
         else:
@@ -68,8 +69,7 @@ def extract_list_of_mileposts(route_name, direction):
     df = df[df.direction == direction]
     df.reset_index(drop=True, inplace=True)
     n = len(df)
-    file_number = 0
-    # file_number = random.randint(0, n-1)
+    file_number = random.randint(0, n-1)
     x = df.file_adr[file_number]
     df = pd.read_excel(x)
     mileposts = list(df.columns[1:])
@@ -85,7 +85,7 @@ metadata = _create_meta()
 
 
 if __name__ == '__main__':
-    file_names = sorted(glob.glob('.\\data\\DriveNet\\*\\*.xlsx'))
+    file_names = sorted(glob.glob('./data/DriveNet/*/*.xlsx'))
 
     info = {'file_adr': [], 'type': [], 'route': [], 'mile_start': [], 'mile_end': [],
             'weekend': [], 'direction': [], 'start_date': [], 'end_date': []}
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     # for i, name in enumerate(file_names):
     for name in file_names:
         org_name = name
-        name = name.split('\\')
+        name = name.split('/')
         pre_name = name[:-1]
         name = name[-1][:-5]  # remove '.xlsx' part
         route, direction, mile_start, mile_end, weekend, start, end = _get_metadata(name)
@@ -115,4 +115,4 @@ if __name__ == '__main__':
     metadata = pd.DataFrame(data=info)
     # metadata.start_date = pd.to_datetime(metadata.start_date)
     # metadata.end_date = pd.to_datetime(metadata.end_date)
-    metadata.to_excel('.\\data\\metadata.xlsx')
+    metadata.to_excel('./data/metadata.xlsx')
