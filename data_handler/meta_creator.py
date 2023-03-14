461ec3ae50e85ddaa3d7452e27bd21fbd795b54a
Bluesoheil99 <soheil.k.78@gmail.com>
2023-03-13 21:16:30 -0700

    a little change in meta_creator.py

--- data_handler/meta_creator.py
+++ data_handler/meta_creator.py
@@ -4,6 +4,10 @@ import os
 import random
 from datetime import datetime
 
+# lines below should be changed based on OS (mac or windows)
+GLOB_ADDRESS = './data/DriveNet/*/*.xlsx'
+SPLITTER = '/'
+
 
 def _define_route(route):
     if route == '005':
@@ -32,7 +36,7 @@ def _get_metadata(file_name):
     return route, direction, mile_start, mile_end, weekend, start, end
 
 
-def _create_meta(glob_address='./data/DriveNet/*/*.xlsx'):
+def _create_meta(glob_address=GLOB_ADDRESS):
     print('metacreator dir: '+os.getcwd())
     file_names = sorted(glob.glob(glob_address))
 
@@ -41,7 +45,7 @@ def _create_meta(glob_address='./data/DriveNet/*/*.xlsx'):
     # for i, name in enumerate(file_names):
     for name in file_names:
         org_name = name
-        name = name.split('/')
+        name = name.split(SPLITTER)
         pre_name = name[:-1]
         name = name[-1][:-5]  # remove '.xlsx' part
         route, direction, mile_start, mile_end, weekend, start, end = _get_metadata(name)
@@ -84,35 +88,35 @@ def get_meta():
 metadata = _create_meta()
 
 
-if __name__ == '__main__':
-    file_names = sorted(glob.glob('./data/DriveNet/*/*.xlsx'))
-
-    info = {'file_adr': [], 'type': [], 'route': [], 'mile_start': [], 'mile_end': [],
-            'weekend': [], 'direction': [], 'start_date': [], 'end_date': []}
-
-    # for i, name in enumerate(file_names):
-    for name in file_names:
-        org_name = name
-        name = name.split('/')
-        pre_name = name[:-1]
-        name = name[-1][:-5]  # remove '.xlsx' part
-        route, direction, mile_start, mile_end, weekend, start, end = _get_metadata(name)
-
-        # info['id'].append(i)
-        info['file_adr'].append(org_name)
-        info['type'].append(pre_name[-1])
-        info['route'].append(route)
-        info['mile_start'].append(mile_start)
-        info['mile_end'].append(mile_end)
-        info['direction'].append(direction)
-        info['start_date'].append(start)
-        info['end_date'].append(end)
-        if weekend == 'MoTuWeThFr':
-            info['weekend'].append(False)
-        else:
-            info['weekend'].append(True)
-
-    metadata = pd.DataFrame(data=info)
-    # metadata.start_date = pd.to_datetime(metadata.start_date)
-    # metadata.end_date = pd.to_datetime(metadata.end_date)
-    metadata.to_excel('./data/metadata.xlsx')
+# if __name__ == '__main__':
+#     file_names = sorted(glob.glob('./data/DriveNet/*/*.xlsx'))
+#
+#     info = {'file_adr': [], 'type': [], 'route': [], 'mile_start': [], 'mile_end': [],
+#             'weekend': [], 'direction': [], 'start_date': [], 'end_date': []}
+#
+#     # for i, name in enumerate(file_names):
+#     for name in file_names:
+#         org_name = name
+#         name = name.split('/')
+#         pre_name = name[:-1]
+#         name = name[-1][:-5]  # remove '.xlsx' part
+#         route, direction, mile_start, mile_end, weekend, start, end = _get_metadata(name)
+#
+#         # info['id'].append(i)
+#         info['file_adr'].append(org_name)
+#         info['type'].append(pre_name[-1])
+#         info['route'].append(route)
+#         info['mile_start'].append(mile_start)
+#         info['mile_end'].append(mile_end)
+#         info['direction'].append(direction)
+#         info['start_date'].append(start)
+#         info['end_date'].append(end)
+#         if weekend == 'MoTuWeThFr':
+#             info['weekend'].append(False)
+#         else:
+#             info['weekend'].append(True)
+#
+#     metadata = pd.DataFrame(data=info)
+#     # metadata.start_date = pd.to_datetime(metadata.start_date)
+#     # metadata.end_date = pd.to_datetime(metadata.end_date)
+#     metadata.to_excel('./data/metadata.xlsx')
